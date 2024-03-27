500b4d515c28e979a873b62fd115697344e8d67a
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-03-27 15:22:03 -0500

    Add health check endpoint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import json
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/')
+def health_check():
+    response = {
+        'status': 'OK'
+    }
+    return json.dumps(response), 200
 
 if __name__=='__main__':
     app.run(debug=True, port=5001)
